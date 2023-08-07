24c2be8d68988a3420ed4fbebd09765badec47e3
John <johpaz252@gmail.com>
2023-08-07 15:18:34 -0500

    undmedida

---
+++ src/dataFake/undMedida.js
@@ -0,0 +1,59 @@
+[
+    {
+      "nombreUnidad": "Unidad",
+      "simboloUnidad": "u",
+      "conversion": 1
+    },
+    {
+      "nombreUnidad": "Kilogramo",
+      "simboloUnidad": "kg",
+      "conversion": 1
+    },
+    {
+      "nombreUnidad": "Gramo",
+      "simboloUnidad": "g",
+      "conversion": 0.001  // 1 kg = 1000 g
+    },
+    {
+      "nombreUnidad": "Litro",
+      "simboloUnidad": "L",
+      "conversion": 1
+    },
+    {
+      "nombreUnidad": "Mililitro",
+      "simboloUnidad": "mL",
+      "conversion": 0.001  // 1 L = 1000 mL
+    },
+    {
+      "nombreUnidad": "Caja",
+      "simboloUnidad": "caja",
+      "conversion": 1
+    },
+    {
+      "nombreUnidad": "Docena",
+      "simboloUnidad": "dz",
+      "conversion": 12  // 1 docena = 12 unidades
+    },
+    {
+      "nombreUnidad": "Paquete",
+      "simboloUnidad": "pkt",
+      "conversion": 1
+    },
+    {
+      "nombreUnidad": "Bulto",
+      "simboloUnidad": "bulto",
+      "conversion": 1
+    },
+    {
+      "nombreUnidad": "Metro",
+      "simboloUnidad": "m",
+      "conversion": 1
+    },
+    {
+      "nombreUnidad": "Centímetro",
+      "simboloUnidad": "cm",
+      "conversion": 0.01  // 1 m = 100 cm
+    }
+  
+  ]
+  
